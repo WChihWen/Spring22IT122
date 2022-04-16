2952e252b5beeb1f49a57f5588c4283081343e8c
WChihWen <yaman0923@gmail.com>
2022-04-16 13:14:37 -0700

    update week2

---
+++ data.js
@@ -0,0 +1,19 @@
+let users = [
+    { id: 1, name : "Jayson", age : 39,  gender: "male", state:"WA"},
+    { id: 2, name : "Jessie", age : 33,  gender: "female", state:"NY"},
+    { id: 3, name : "Jenny", age : 40,  gender: "female", state:"WA"},
+    { id: 4, name : "Jack", age : 29,  gender: "male", state:"NY"},
+    { id: 5, name : "Reder", age : 41,  gender: "male", state:"NY"}
+    ];
+
+
+export const getAll = () => {
+    return users;
+};
+
+export const getItem = (name) => {
+    return users.find((element) => {
+        return element.name == name;
+    });
+};
+
